a9281a190a70b540ee5ebe051f11809a0b368ba7
Alonso Cedeño Sánchez <acsanchez2909@gmail.com>
2022-08-31 17:03:12 -0600

    .

--- Scripts/header-snippet.js
+++ Scripts/header-snippet.js
@@ -41,3 +41,47 @@ function includeHeaderSnippet() {
         }
     }
 };
+
+function includeSubHeaderSnippet() {
+
+    // Traverse the collection of all
+    // HTML elements
+    id = document.getElementsByTagName("*");
+    for (i = 0; i < id.length; i++) {
+        element = id[i];
+
+        // Search for elements with
+        // specific attributes
+        file = element.getAttribute(
+            "sub-header-snippet");
+
+        if (file) {
+              
+            // Create an HTTP request with 
+            // the attribute value as the
+            // file name
+            xmlRequest = new XMLHttpRequest();
+            xmlRequest.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { 
+                        element.innerHTML = this.responseText; 
+                    }
+                      
+                    if (this.status == 404) { 
+                        element.innerHTML = "Page not found."; 
+                    }
+                      
+                    // Delete the attribute and
+                    // call this function again.
+                    element.removeAttribute(
+                        "sub-header-snippet");
+
+                    includeSubHeaderSnippet();
+                }
+            }
+            xmlRequest.open("GET", file, true);
+            xmlRequest.send();
+            return; // Exit function.
+        }
+    }
+};
